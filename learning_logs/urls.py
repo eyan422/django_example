1012ecb2824e76f5054af15ffe14f2f8c81a6419
Yan Feng <frankie.yanfeng@gmail.com>
2024-05-05 23:05:34 +0800

    add comment

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,5 +7,6 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+    # Pages that shows all topics
     path('topics/', views.topics, name='topics'),
 ]
